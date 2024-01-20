bd79e352e1ce5a345142afe35b5f65c534fc13b3
Faridun <faridun.mirzoev@gmail.com>
2024-01-20 00:18:40 -0800

    update vite version

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,5 +1,6 @@
-import React from "react";
 
+
+// eslint-disable-next-line react/prop-types
 const Button = ({ label, value, onClick, styles }) => {
   return (
     <button onClick={() => onClick(value)} className={styles}>
